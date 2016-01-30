f7af250e43f451718f17e10f27129f17cb47152c
MKS-students <mks.students@hackreactor.com>
2016-01-30 14:10:22 -0600

    first commit

--- Exercise-1.js
+++ Exercise-1.js
@@ -6,18 +6,20 @@
 //factorial of 4 = 4 x 3 x 2 x 1 = 24
 
 function factorial(n){
-
+  if(n <= 1) return n;
+  return factorial(n-1) * n;
 }
 
 //Sequence 2, 4, 6, 8, 10, ...
-function nameYourFunction(){
-
-}
+function Add2(n){
+  if( n = 100 ) return n;
+  return Add2(n+2);
+} // not finished
 
 
 //Sequence 3, 9, 27, 81, ...
 function nameYourFunction(){
-
+  return factorial(n*3 * 3;
 }
 
 
